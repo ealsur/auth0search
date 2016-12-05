e287687a9074f0f03e20a02b11cbfa4a31886890
ealsur <ealsur@hotmail.com>
2016-12-05 20:52:50 -0300

    Adding custom fields

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,15 +1,27 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authorization;
+using auth0search.Extensions;
 
 namespace auth0search.Controllers
 {
     public class AccountController : Controller
     {
+        IOptions<OpenIdConnectOptions> _options;
+
+        public AccountController(IOptions<OpenIdConnectOptions> options)
+        {
+            _options = options;
+        }
+
         public IActionResult Login(string returnUrl = "/")
         {
-            return new ChallengeResult("Auth0", new AuthenticationProperties() { RedirectUri = returnUrl });
+            var lockContext = HttpContext.GenerateLockContext(_options.Value, returnUrl);
+
+            return View(lockContext);
         }
 
         [Authorize]
